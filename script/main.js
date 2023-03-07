554473217964ffe12d18ba2f2f91831e2f47a0d6
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-03-06 22:58:53 -0300

    06032023

--- script/main.js
+++ script/main.js
@@ -15,8 +15,8 @@ function backTop(){
 /*texto do banner*/
 
 const texto = document.querySelector("#texto");
-const textoDigitado = "Olá me chamo Neomar. Precisa de um programador! Como posso lhe ajudar?";
-const intervalo = 300;
+const textoDigitado = "A criatividade é a inteligência se divertindo.";
+const intervalo = 200;
 
 function showTexto (texto, textoDigitado, intervalo){
     const char = textoDigitado.split("").reverse();
@@ -33,3 +33,50 @@ function showTexto (texto, textoDigitado, intervalo){
 }
 
 showTexto(texto, textoDigitado, intervalo);
+
+
+
+/*<button id="btn-topo">Voltar ao Topo</button>
+
+
+#btn-topo {
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    font-size: 18px;
+    border: none;
+    outline: none;
+    background-color: #555;
+    color: white;
+    cursor: pointer;
+    padding: 15px;
+    border-radius: 10px;
+  }
+
+  // Obtenha o botão
+var btnTopo = document.getElementById("btn-topo");
+
+// Quando o usuário rolar a página para baixo 20px do topo do documento, mostre o botão
+window.onscroll = function() {
+  mostrarBotaoTopo()
+};
+
+function mostrarBotaoTopo() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnTopo.style.display = "block";
+  } else {
+    btnTopo.style.display = "none";
+  }
+}
+
+// Quando o usuário clicar no botão, role de volta ao topo do documento
+function voltarAoTopo() {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
+  }
+  btnTopo.onclick = function() {
+    voltarAoTopo();
+  }
+  como fazer um botão que com um click ele me retorna para o top 0 do site utilizando html, css e javascript*/  
